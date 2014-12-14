60172d6335df04d7bcd7129d6272539b499d3d89
Liam <GrandStave@users.noreply.github.com>
2014-12-14 14:17:00 -0500

    Update Input.java

--- src/com/frc869/robot/preSeason2015/subsystems/Input.java
+++ src/com/frc869/robot/preSeason2015/subsystems/Input.java
@@ -29,11 +29,33 @@ public class Input implements IInput {
         logger = Logging.getInstance();
     }
     public double getJoystick(boolean rightStick, boolean yAxis) {
+        /**(true true) would be yAxis movement on the righStick
+         * (true false) would be xAxis movement on rightStick
+         * (false z) would be z movement on the leftStick, etc.
+         * the double is the the joystick movement from -1 to 1
+         * if double is 0,no movement; >0 is foward, <0 is backward
+         */
+        
         logger.log(Logging.wtf, TAG, "Not supported yet.");
         return 0;
     }
 
     public boolean getButton(int buttonNum) {
+        if (true)
+            {if (buttonNum = 0)
+                {//preform function of aButton
+                };
+            else if (buttonNum = 1)
+                {//preform function of bButton
+                };
+            else if (buttonNum = 2)
+                {//etc.
+                };
+            }
+        else
+            {//keep checking
+            }
+    
         logger.log(Logging.wtf, TAG, "Not supported yet.");
         return false;
     }
