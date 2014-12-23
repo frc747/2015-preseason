e7ad0ed50494712a845e777cfebcbdde17a1b99f
Michael Chinn <git@mechinn.com>
2014-12-23 10:40:50 -0500

    Added watchdog and prototypes for the other modes in IterativeRobot

--- src/com/frc869/robot/preSeason2015/Robot.java
+++ src/com/frc869/robot/preSeason2015/Robot.java
@@ -15,6 +15,7 @@ import com.frc869.robot.preSeason2015.subsystems.Move;
 import com.frc869.robot.preSeason2015.subsystems.Spikes;
 import com.frc869.robot.preSeason2015.subsystems.interfaces.IInput;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
@@ -33,7 +34,12 @@ public class Robot extends IterativeRobot {
     private Spikes spikes;
     private IInput controller;
     
-    //start up the robot
+    /**
+     * Robot-wide initialization code should go here.
+     *
+     * Users should override this method for default Robot-wide initialization which will
+     * be called when the robot is first powered on.  It will be called exactly 1 time.
+     */
     public void robotInit() {
         logger = Logging.getInstance();
         logger.log(Logging.info, TAG, "Starting robot");
@@ -44,16 +50,99 @@ public class Robot extends IterativeRobot {
         controller = LogitechGamepad.getInstance();
         encoder.setup();
         limits.setup();
+        this.getWatchdog().setExpiration(5);
+        this.getWatchdog().setEnabled(true);
         logger.log(Logging.info, TAG, "Done starting robot");
     }
+
+    /**
+     * Initialization code for disabled mode should go here.
+     *
+     * Users should override this method for initialization code which will be called each time
+     * the robot enters disabled mode.
+     */
+    public void disabledInit() {
+        logger.log(Logging.info, TAG, "Disabled");
+    }
+
+    /**
+     * Initialization code for autonomous mode should go here.
+     *
+     * Users should override this method for initialization code which will be called each time
+     * the robot enters autonomous mode.
+     */
+    public void autonomousInit() {
+        logger.log(Logging.info, TAG, "Use test mode not autonomous");
+    }
+
+    /**
+     * Initialization code for teleop mode should go here.
+     *
+     * Users should override this method for initialization code which will be called each time
+     * the robot enters teleop mode.
+     */
+    public void teleopInit() {
+        logger.log(Logging.info, TAG, "Use test mode not teleoperated");
+    }
     
+    /**
+     * Initialization code for test mode should go here.
+     * 
+     * Users should override this method for initialization code which will be called each time
+     * the robot enters test mode.
+     */
+    public void testInit() {
+        
+    }
+    
+    /**
+     * Periodic code for disabled mode should go here.
+     *
+     * Users should override this method for code which will be called periodically at a regular
+     * rate while the robot is in disabled mode.
+     */
+    public void disabledPeriodic() {
+        this.getWatchdog().feed();
+        Timer.delay(0.001);
+    }
+
+    /**
+     * Periodic code for autonomous mode should go here.
+     *
+     * Users should override this method for code which will be called periodically at a regular
+     * rate while the robot is in autonomous mode.
+     */
+    public void autonomousPeriodic() {
+        this.getWatchdog().feed();
+        Timer.delay(0.001);
+    }
+
+    /**
+     * Periodic code for teleop mode should go here.
+     *
+     * Users should override this method for code which will be called periodically at a regular
+     * rate while the robot is in teleop mode.
+     */
+    public void teleopPeriodic() {
+        this.getWatchdog().feed();
+        Timer.delay(0.001);
+    }
+    
+    /**
+     * Periodic code for test mode should go here
+     * 
+     * Users should override this method for code which will be called periodically at a regular rate
+     * while the robot is in test mode.
+     */
     //treat this as teleoperated, it gives is amazing output on smartdashboard :)
     public void testPeriodic() {
         logger.log(Logging.info, TAG, "Test Loop Start");
+        this.getWatchdog().feed();
         move.control(controller);
         spikes.control(controller);
         SmartDashboard.putNumber("Left encoder distance", encoder.getDistance(false));
         SmartDashboard.putNumber("Right encoder distance", encoder.getDistance(true));
+        Timer.delay(0.001);
         logger.log(Logging.info, TAG, "Test Loop End");
     }
     
